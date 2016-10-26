4b8bf7ae4db47fefac2aa88d59a5ded01e5a0c3f
Chuk <chukmunnlee@gmail.com>
2016-10-26 18:50:04 +0800

    workshop03 - not complete

---
+++ workshop03/src/java/beauj/workshop03/business/CustomerBean.java
@@ -0,0 +1,30 @@
+package beauj.workshop03.business;
+
+import beauj.workshop03.model.Customer;
+import java.util.List;
+import java.util.Optional;
+import javax.ejb.Stateless;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@Stateless
+public class CustomerBean {
+
+	@PersistenceContext private EntityManager em;
+
+	public Optional<Customer> findById(Integer cid) {
+		return (Optional.ofNullable(em.find(Customer.class, cid)));
+	}
+
+	public List<Customer> findAll() {
+		TypedQuery<Customer> query = em.createNamedQuery("Customer.findAll", Customer.class);
+		return (query.getResultList());
+	}
+
+	public void add(Customer customer) {
+		em.persist(customer);
+	}
+	
+}
---
+++ workshop03/src/java/beauj/workshop03/rest/CustomerResource.java
@@ -0,0 +1,97 @@
+package beauj.workshop03.rest;
+
+import beauj.workshop03.business.CustomerBean;
+import beauj.workshop03.model.Customer;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.persistence.EntityExistsException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+@RequestScoped
+@Path("/customer")
+@Produces(MediaType.APPLICATION_JSON)
+public class CustomerResource {
+
+	@EJB private CustomerBean customerBean;
+
+	@GET
+	@Path("{cid}")
+	public Response get(@PathParam("cid") Integer customerId) {
+
+		Optional<Customer> opt = customerBean.findById(customerId);
+
+		if (opt.isPresent())
+			return (Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Customer %d not found", customerId))
+					.build());
+		return (Response.ok(opt.get().toJSON()).build());
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response post(MultivaluedMap<String, String> formData) {
+
+		Customer customer = new Customer();
+		customer.setCustomerId(Integer.parseInt(formData.getFirst("custId")));
+		customer.setName(formData.getFirst("name"));
+		customer.setAddressline1(formData.getFirst("addr1"));
+		customer.setAddressline2(formData.getFirst("addr2"));
+		customer.setCity(formData.getFirst("city"));
+		customer.setState(formData.getFirst("state"));
+		customer.setPhone(formData.getFirst("phone"));
+		customer.setFax(formData.getFirst("fax"));
+		customer.setEmail(formData.getFirst("email"));
+		customer.setDiscountCode(formData.getFirst("discount"));
+		customer.setCreditLimt(Integer.parseInt(formData.getFirst("credit")));
+
+		try {
+			customerBean.add(customer);
+		} catch (EntityExistsException ex) {
+			return (Response.status(Response.Status.BAD_GATEWAY)
+					.entity(ex.getMessage()).build());
+		} catch (Throwable t) {
+			return (Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+					.entity(t.getMessage()).build());
+		}
+
+		return (Response.status(Response.Status.CREATED).build());
+	}
+
+	@GET
+	public Response get(@Context UriInfo ui) {
+
+		UriBuilder builder = ui.getBaseUriBuilder()
+				.path(CustomerResource.class);
+
+		try {
+			builder = builder.path(CustomerResource.class.getMethod("get", Integer.class));
+		} catch (Exception ex) { }
+
+		JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+		List<Customer> customers = customerBean.findAll();
+
+		for (Customer c: customers) {
+			URI uri = builder.clone().build(c.getCustomerId());
+			arrBuilder.add(uri.toString());
+		}
+
+		return (Response.ok(arrBuilder.build()).build());
+	}
+	
+}
